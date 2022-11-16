fe9c1b11f9c2c9dcc6f61272312b4c9b611ee7c1
Wairimu2018 <wairimukuria.wk@gmail.com>
2022-11-16 13:03:08 +0300

    InfoSection

---
+++ src/Components/HeroSection/index.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import {
+    HeroContainer,
+    // HeroBg,
+    // VideoBg,
+    HeroContent,
+    HeroBtnWrapper,
+    ArrowForward,
+    ArrowRight,
+} from "./HeroSectionElement";
+import { ButtonR } from "./ButtonElements";
+// import Video from "./videos/bgvideo.mp4";
+import "../..//App.css";
+import { hover } from "@testing-library/user-event/dist/hover";
+
+const HeroSection =() => {
+    cont [hover, setHover] = useState(false);
+
+    const onHover =() => {
+        setHover(!hover);
+    };
+
+    return (
+        <HeroContainer>
+            <HeroContent>
+                <h1 className="h1-hero">Empowering Livelihoods</h1>
+                <p className="hero-text">
+                Sign up today and recieve $250 in credit for your next sucessfull
+          project.
+                </p>
+                <HeroBtnWrapper>
+                <ButtonR to="/signin" onMouseEnter={onHover} onMouseLeave={onHover} primary="true" dark="true">
+            Get started {hover ? <ArrowForward /> : <ArrowRight />}
+          </ButtonR>
+                </HeroBtnWrapper>
+            </HeroContent>
+        </HeroContainer>
+    );
+};
+
+export default HeroSection;
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,14 +1,19 @@
-import React, { useState} from "react";
+// import React, { useState} from "react";
 import Footer from "../Components/Footer";
+// import Services from "../Components/Services";
 
 export const Home = () => {
-    const [isOpen, setIsOpen] = useState(false)
+    // const [isOpen, setIsOpen] = useState(false)
 
     // const toggle = () => {
     //     setIsOpen(!isOpen);
     // };
 
     return (
+//       <>
+//       <Services/>
+
+// </>  
         <Footer />
     );
 };
