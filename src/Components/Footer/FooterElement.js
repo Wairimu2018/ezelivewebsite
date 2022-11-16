466119a5680aeaa7efd6bd977782e2d2a074683a
Wairimu2018 <wairimukuria.wk@gmail.com>
2022-11-16 08:08:48 +0300

    footer section

---
+++ src/Components/Footer/FooterElement.js
@@ -0,0 +1,37 @@
+import { findNonSerializableValue } from "@reduxjs/toolkit";
+import styled from "styled-components";
+
+export const FooterContainer = styled.div`
+padding-top: 30px;
+padding-bottom: 15px;
+background: linear-gradient(#150b1f, #000);
+
+`
+
+export const FooterWrapper = styled.div`
+max-width: 900px;
+
+`
+
+export const FooterH2 = styled.h2`
+font-size: 0.9rem;
+text-decoration; none;
+color: white;
+font-weigt: 5000;
+margin-bottom: 30px
+
+`
+
+export const FooterLink = styled.a`
+font-size: 0.9rem;
+text-decoration: none;
+color: white;
+display: flex;
+transition: all 0.3s;
+
+ &:hover {
+    color: #B05FFD;
+ }
+
+`
+
